ae751fc806da1b3f5b42393cda34e376d64271c2
Debora <116604446+deborabckp@users.noreply.github.com>
2023-02-03 23:30:28 -0300

    Create lista1_ponteiro.c

---
+++ ponteiro/lista1_ponteiro.c
@@ -0,0 +1,27 @@
+//Exercício 1: Quais serão os valores de x,y e p, ao final do trecho de código abaixo:
+
+#include<stdio.h> 
+
+int main(){ 
+
+    int x, y, *p; //declaração de variáveis inteiras, 'x' e 'y', e um ponteiro 'p' do tipo inteiro
+
+    y=0; //a variável y recebe o valor 0
+
+    p=&y; //o ponteiro 'p' recebe o endereço de y
+
+    x=*p; //x recebe o conteúdo de p, que é 0, pois p está apontado para y, então o conteúdo dele é o que está em y
+
+    x=4; //x que antes era 0, agora recebe 4
+
+    (*p)++; //o conteúdo de p recebe um incremento, que é a mesma coisa de y+1
+
+    --x; //o que está em x recebe um decremento, que é o que tem em x-1
+
+    (*p)+= x; //o conteúdo de p, recebe o que está em p mais o que está em x, (*p+x)
+
+    printf("%d %d %p\n",x, y, p); //mostrando o valor de x, y e p
+
+return 0;
+
+} 
